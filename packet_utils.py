33425dc65453d540bd5e5d6089ccf5f5102b3385
Andrew Taylor <rolyata47@gmail.com>
2025-09-28 15:38:02 -0500

    Adding decoded message to the logs

--- packet_utils.py
+++ packet_utils.py
@@ -1,3 +1,5 @@
+import packet_types
+
 def hexdump(packet):
     packet_len = len(packet)
     index = 0
@@ -20,4 +22,16 @@ def hexdump(packet):
         line = format("%s %s\n" %(index_str, " ".join(words)))
         contents += line
         index += 16
+    return contents
+
+def str(packet):
+    packet_len = len(packet)
+    packet_type = packet[4]
+    packet_type = packet_types.map[packet_type] if packet_type in packet_types.map else "UNKNOWN"
+    packet_body = packet[5:packet_len].decode("utf-8")
+    contents = format("Packet length: %d bytes\n" %packet_len)
+    contents += format("Packet type: %s\n" %packet_type)
+    contents += format("Packet body: %s" %packet_body)
+    if not contents.endswith("\n"):
+        contents += "\n"
     return contents
